690e569224211076f17ec1d8b23118061fb5a046
Daniils Fjodorovs <daniilfedorov@Daniilss-MacBook-Pro.local>
2026-06-06 20:56:00 +0200

    Demosntration of genetarots use to recreate range class

--- algorithms_and_data_structures/Goodrich_book/OOP/iterators.py
+++ algorithms_and_data_structures/Goodrich_book/OOP/iterators.py
@@ -24,3 +24,50 @@ if __name__ == "__main__":
     print(next(my_iterator))
     print(next(my_iterator))
     print(next(my_iterator))
+
+
+#__________Example" Range Class that mimics the built-in range class
+r = range(8,140,5)
+print(type(r))
+print(r)
+print(r[0])
+print(r[0:5:2])
+print(len(r))
+
+class Range:
+    """A class that mimics the built-in range class
+    Shows the implelmentation of generators as 
+    len will automatically implemnt the iterator protocol"""
+
+    def __init__(self, start, stop, step):
+        """Initialize a Range instance.
+        
+        Semantics is similar to built-in range class"""
+        if step == 0:
+            raise ValueError("step cannot be 0")
+
+        if stop is None: #Special case of range(n)
+            start, stop = 0, start #Will return in range (0, n)
+        
+        #calculate the effective length once
+
+        self._length = max(0, (stop - start + step - 1) // step)
+
+        #Need knowledge of start and step (but not stop_ to support __getitem__)
+        self._start = start
+        self._step = step
+
+    def __len__(self):
+        """Return the number of entrieus in the range"""
+        return self._length
+
+    def __getitem__(self, k):
+        """Return entry at index k (using standard interpretation if negative)"""
+        if k < 0:
+            k += len(self) #attempt to convert negative index
+
+        if not 0 <= k < self._length:
+            raise IndexError("index out of range")
+        
+        return self._start + k * self._step
+    
